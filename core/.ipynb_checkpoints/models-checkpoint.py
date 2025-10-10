d10f8696febfa571a3b45e9f7e9f842e10657de6
yejinBang <aaasd143@naver.com>
2025-10-10 22:06:34 +0900

    채팅 api 구현 및 models 수정

--- core/.ipynb_checkpoints/models-checkpoint.py
+++ core/.ipynb_checkpoints/models-checkpoint.py
@@ -1,9 +1,6 @@
 # DB 테이블과 매핑되는 SQLAlchemy 모델을 정의
-from sqlalchemy import Column, Integer, String, Date, Time
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-import datetime
-from core.db import Base
 
 Base = declarative_base()
 
@@ -14,7 +11,5 @@ class User(Base):
     firebase_uid = Column(String(128), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     nickname = Column(String(50))
+    birthdate = Column(DateTime, nullable=False)
     gender = Column(String(1), nullable=False)
-    birth_date = Column(Date, nullable=False)
-    birth_time = Column(Time, nullable=True)
-    birth_calendar = Column(String(20), nullable=False, default="solar")
