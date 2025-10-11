7655e3af733da88664fdb630cef6edf1be742655
yooseohyeon <yooseohyeon.02@gmail.com>
2025-10-11 22:30:06 +0900

    Feat: 오행 비율 저장을 위한 User 모델 오행 필드 FLOAT 타입 변경 및 Manse 테이블 추가

--- core/models.py
+++ core/models.py
@@ -1,11 +1,6 @@
 # DB 테이블과 매핑되는 SQLAlchemy 모델을 정의
-from sqlalchemy import Column, Integer, String, Date, Time
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-import datetime
+from sqlalchemy import Column, Integer, String, Date, Time, DateTime, Boolean, Float
 from core.db import Base
-
-Base = declarative_base()
 
 class User(Base):
     __tablename__ = "Users"
@@ -19,3 +14,25 @@ class User(Base):
     birth_time = Column(Time, nullable=True)
     birth_calendar = Column(String(20), nullable=False, default="solar")
     profile_image = Column(String(255), nullable=True)
+    oheng_wood = Column(Float, nullable=True)
+    oheng_fire = Column(Float, nullable=True)
+    oheng_earth = Column(Float, nullable=True)
+    oheng_metal = Column(Float, nullable=True)
+    oheng_water = Column(Float, nullable=True)
+
+class Manse(Base):
+    __tablename__ = "manses"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    solarDate = Column(Date, nullable=False)
+    lunarDate = Column(Date, nullable=False)
+    season = Column(String(10))
+    seasonStartTime = Column(DateTime)
+    leapMonth = Column(Boolean)
+    yearSky = Column(String(10))
+    yearGround = Column(String(10))
+    monthSky = Column(String(10))
+    monthGround = Column(String(10))
+    daySky = Column(String(10))
+    dayGround = Column(String(10))
+    
