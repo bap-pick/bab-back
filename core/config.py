3c240e65f4b738858da0514bcd4e8526785252cb
yooseohyeon <yooseohyeon.02@gmail.com>
2025-10-15 18:45:29 +0900

    feat: AWS S3 연동을 위해 관련 환경 변수 추가

--- core/config.py
+++ core/config.py
@@ -12,3 +12,8 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT", 3306)
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+AWS_S3_REGION = os.getenv("AWS_S3_REGION")
