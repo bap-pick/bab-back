cfbd4ddf729a4cc75b92e56e0350a32fd5bd38e3
yooseohyeon <yooseohyeon.02@gmail.com>
2025-10-22 19:30:36 +0900

    feat: 사용자 uid 검증 코드 추가

--- api/chat.py
+++ api/chat.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from core.db import get_db
-from core.models import ChatRoom, ChatMessage
+from core.models import ChatRoom, ChatMessage, User
 from core.firebase_auth import verify_firebase_token
 import datetime
-import os
-from dotenv import load_dotenv
 from typing import Optional
 from google import genai
 from google.genai import types
@@ -28,9 +26,6 @@ class ChatRoomCreateRequest(BaseModel):
 
 Chat_rooms = {}
 
-load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMMA_API_KEY"))
-
 # ---------------- 채팅방 생성 ----------------
 @router.post("/create")
 async def create_chatroom(
@@ -38,9 +33,13 @@ async def create_chatroom(
     uid: str = Depends(verify_firebase_token),
     db: Session = Depends(get_db)
 ):
-
-    chatroom = ChatRoom(name=data.name, is_group=data.is_group)
+    # DB에서 사용자 조회 및 검증
+    user = db.query(User).filter(User.firebase_uid == uid).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="등록되지 않은 사용자입니다.")
     
+    # 채팅방 생성 및 저장
+    chatroom = ChatRoom(name=data.name, is_group=data.is_group)
     db.add(chatroom)
     db.commit()
     db.refresh(chatroom) # DB에서 자동 생성된 chatroom.id를 가져옴
@@ -56,6 +55,11 @@ async def list_chatrooms(
     uid: str = Depends(verify_firebase_token),
     db: Session = Depends(get_db)
 ):
+    # DB에서 사용자 조회 및 검증
+    user = db.query(User).filter(User.firebase_uid == uid).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="등록되지 않은 사용자입니다.")
+    
     rooms = db.query(ChatRoom).all()
     return [
         {"id": room.id, "name": room.name, "created_at": room.created_at}
@@ -69,6 +73,12 @@ async def delete_chatroom(
     uid: str = Depends(verify_firebase_token),
     db: Session = Depends(get_db)
 ):
+    # DB에서 사용자 조회 및 검증
+    user = db.query(User).filter(User.firebase_uid == uid).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="등록되지 않은 사용자입니다.")
+    
+    # DB에서 채팅방 조회
     room = db.query(ChatRoom).filter(ChatRoom.id == room_id).first()
     if not room:
         raise HTTPException(status_code=404, detail="채팅방을 찾을 수 없음")
@@ -88,7 +98,12 @@ async def send_message(
     uid: str = Depends(verify_firebase_token),
     db: Session = Depends(get_db)
 ):
-    # 채팅방 확인
+    # DB에서 사용자 조회 및 검증
+    user = db.query(User).filter(User.firebase_uid == uid).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="등록되지 않은 사용자입니다.")
+    
+    # DB에서 채팅방 확인
     chatroom = db.query(ChatRoom).filter(ChatRoom.id == request.room_id).first()
     if not chatroom:
         raise HTTPException(status_code=404, detail="채팅방을 찾을 수 없음")
